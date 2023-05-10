5a4386a273122d88ced75c79368801fdc40d4906
garyhui <gary.hui@sendum.com>
2023-05-10 15:00:15 -0700

    Display basic weather info from Vancouver

--- weatherapp/src/components/currentWeather.jsx
+++ weatherapp/src/components/currentWeather.jsx
@@ -1,16 +1,56 @@
 import { useState, useEffect } from 'react'
 import { getWeather } from '@/pages/api/weatherAPI'
+import { Grid, Typography } from '@mui/material'
 
 export default function CurrentWeather() {
+  const [weatherData, setWeatherData] = useState()
+  let weatherObj = {}
   useEffect(() => {
-    getWeather()
+    // define async/await function to do
+    const weather = async () => {
+      setWeatherData(await getWeather())
+      console.log({ weatherObj }, `hi there${''}`)
+    }
+    // /run it
+    weather()
+    // this gets called when component unmounts
+    return () => {}
   }, [])
 
-  return (
-    <div>
-      <h1>Current Weather</h1>
+  let sunrise = new Date(weatherData?.sys.sunrise * 1000).toLocaleTimeString()
+  let sunset = new Date(weatherData?.sys.sunset * 1000).toLocaleTimeString()
 
-      <img src='https://openweathermap.org/img/wn/13d@2x.png'></img>
-    </div>
+  return weatherData ? (
+    <Grid
+      container
+      direction={'column'}
+      gap={2}
+      justifyContent={'center'}
+      alignItems={'center'}
+    >
+      <Typography variant='h3'>{weatherData.name}</Typography>
+      <Typography variant='h4'>{Math.round(weatherData.main.temp)}</Typography>
+      <div>
+        <img
+          src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+        ></img>
+      </div>
+
+      {/* description */}
+      <div>{weatherData.weather[0].description}</div>
+
+      {/* Day high and low */}
+      <div>
+        H: {weatherData.main.temp_max} L:{weatherData.main.temp_min}
+      </div>
+
+      {/* Sunrise/sunset */}
+      <div>
+        Sunrise/Sunset:{sunrise},{sunset}
+      </div>
+      <div> Hourly Forecast:</div>
+    </Grid>
+  ) : (
+    <div> no weather data</div>
   )
 }
